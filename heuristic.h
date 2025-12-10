5d0c6135acc5507cb433b761a72249a1d9eb0bb0
ZhaojinZhong <1046576137@qq.com>
2025-12-10 17:09:41 +0800

    idea68：12-10-1

--- heuristic.h
+++ heuristic.h
@@ -344,11 +344,11 @@ void DeepDist::local_search_with_decimation(char *inputfile)
         if(local_opt <= opt_unsat_weight){
             if(cutoff_times <= not_opt_times + opt_times){
                 cutoff_times = not_opt_times + opt_times + 1;
-                cout<<"new cutoff_times: "<<cutoff_times<<endl;
+                // cout<<"new cutoff_times: "<<cutoff_times<<endl;
             }
         }
-        cout<<"local_opt :"<<local_opt<<", opt_unsat_weight: "<<opt_unsat_weight<<endl;
-        cout<<"tries: "<<tries<<", opt_times: "<<opt_times<<", not_opt_times: "<<not_opt_times<<endl;
+        // cout<<"local_opt :"<<local_opt<<", opt_unsat_weight: "<<opt_unsat_weight<<endl;
+        // cout<<"tries: "<<tries<<", opt_times: "<<opt_times<<", not_opt_times: "<<not_opt_times<<endl;
     }
 }
 
